afacb5877420c0916017ce29e34f4123067ead4b
Flo <s347326@uniwuerzburg.onmicrosoft.com>
2020-09-22 17:41:29 +0200

    - added plotting functionality - energy is used instead of wavelength - initial values and bounds need to be adjusted

--- Duane_Hunt_Plots.py
+++ Duane_Hunt_Plots.py
@@ -8,45 +8,93 @@ def linear_fit(x, y0, a):
 
     return a*x+y0
 
+def Multi_Gauss_fit(x, *p):
+
+    return (p[0] + p[1]*np.exp(-(x-p[2])**2/(2*p[3]**2)) + p[4] + p[5]*np.exp(-(x-p[6])**2/(2*p[3]**2)))
+
 def reader(dirName):
-    imps = []
-    angle = []
-    wavelength = []
+    imps = [] #recorded counts per seconds
+    angle = [] #measured angle of the goniometer
+    energy = []
     intensity = []
-    tau = 90*E-6
-    int_time = 2
-    d = 201.4E-12
+    tau = 90*10**(-6) #dead time of the Geiger counter
+    int_time = 2 #integration time of the detektor in seconds
+    d = 201.4*10**(-12)
+    h = 6.582119569*10**(-16)
+    c = 299792458
     j = 0 #for skipping the first few lines
     
     for root, dirs , files in os.walk(dirName):
         for file in files:
-            if file.endswith(".txt"):
+            #if '.txt' in file:
                 print("Reading in file " + os.path.join(root, file))
                 with open(os.path.join(root, file)) as f:
                     reader = csv.reader(f, delimiter = '\t')
                     for row in reader:
                         if j > 2:
-                            #print(row[0])
-                            #print(row[1])
+                            angle.append(row[0].replace(',','.'))
                             imps.append(row[1])
-                            angle.append(row[0])
                         j += 1
                 j = 0
-
+    
     f.close()
+
+    imps = list(map(float, imps ))
+    angle = list(map(float, angle ))
 
     for i in range(0,len(angle)):
         intensity.append(imps[i]*int_time/(1-tau*imps[i]*int_time))
-        wavelength.append(2*d*np.sin(angle[i]*np.pi/180))
+        energy.append(h*c/(2*d*np.sin(angle[i]*np.pi/180)))
 
     #for i in range(len(intensity)):
     #    print(intensity[i] + " ; " + wavelength[i])
 
-    return intensity, wavelength
+    return intensity, energy
+
+def plot_spectrum():
+
+    Cu = r"C:\Users\Flo\Desktop\F Praktikum\X Ray\Data\2020-09-22\data\Cu"
+    Test = r"C:\Users\Flo\Desktop\F Praktikum\X Ray\Data\2020-09-22\data\Test"
+    intensity , energy = reader(Test)
+    x_plt = np.empty(len(intensity), dtype = float)
+    y_plt = np.empty(len(intensity), dtype = float)
+    y_err = np.empty(len(intensity), dtype = float)
+    x_min = 0.138
+    x_min1 = 0.153
+    x_max = 0.1575
+
+    y_max = max(intensity)
+
+    for i in range(0, len(intensity)):
+        x_plt[i] = energy[i]/1000
+        y_plt[i] = intensity[i]/y_max
+
+    for i in range(0, len(y_plt)):
+        if y_plt[i] == 0.0:
+            y_err[i] == 0
+        else:
+            y_err[i] = y_plt[i]/(np.sqrt(y_plt[i]))
+
+    initial_values = [0,0.01,x_min,0.04,0,0.01,x_min1]
+    bounds = ([0,0.01,x_min,0.04,0,0,x_min1],[0.07,0.1,0.141,0.09,0.1,0.5,x_max])
+
+    plt.errorbar(x_plt, y_plt, yerr = None, fmt = 'x', markersize = 3 )
+    params, params_cov = scipy.optimize.curve_fit(Multi_Gauss_fit, x_plt, y_plt, p0 = initial_values, bounds = bounds, sigma = None, absolute_sigma = True)
+    plt.plot(x_plt, Multi_Gauss_fit(x_plt, *params))
+
+    print(params[2],"\n" ,params[6])
+
+    plt.xlabel("Energy [keV]")
+    plt.ylabel("Intensity [arbitrary units]")
+    plt.xlim(1.2,1.8)
+    plt.grid()
+    plt.show()
+    plt.clf()
+
+    return 0
 
 def main():
-    dirName = r"C:\Users\Flo\Desktop\F Praktikum\X Ray\Data\DuaneHunt"
-    reader(dirName)
+    plot_spectrum()
 
 if __name__ == "__main__" :
     main()
